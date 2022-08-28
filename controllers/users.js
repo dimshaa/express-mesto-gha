21f352b0bba71ca5144c60d136788cc0a8d62ba6
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-08-28 21:00:57 +0300

    update user routes & controller

--- controllers/users.js
+++ controllers/users.js
@@ -32,8 +32,48 @@ const createUser = (req, res) => {
     });
 };
 
+const updateUserInfo = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: `oh! it's ${err}` });
+    });
+};
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: `oh! it's ${err}` });
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
+  updateUserInfo,
+  updateUserAvatar,
 };
