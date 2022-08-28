1d06c6b7db8055cd1e2ace86f00bda80c487f4be
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-08-28 22:08:00 +0300

    update card routes & controller

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,7 +2,8 @@ const Card = require('../models/card');
 
 const getCards = (req, res) => {
   Card.find({})
-    .populate('owner')
+    .populate('owner', 'name about')
+    .populate('likes', 'name')
     .then((cards) => {
       res.send({ data: cards });
     })
@@ -33,8 +34,38 @@ const deleteCard = (req, res) => {
     });
 };
 
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: `oh! it's ${err}` });
+    });
+};
+
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: `oh! it's ${err}` });
+    });
+};
+
 module.exports = {
   getCards,
   createCard,
   deleteCard,
+  likeCard,
+  dislikeCard,
 };
--- routes/cards.js
+++ routes/cards.js
@@ -4,10 +4,14 @@ const {
   getCards,
   createCard,
   deleteCard,
+  likeCard,
+  dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
 router.post('/cards', createCard);
 router.delete('/cards/:cardId', deleteCard);
+router.put('/cards/:cardId/likes', likeCard);
+router.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = router;
