bcc94399a16ed5cc6a769c0e6e7033c2ac04aff1
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-09-17 15:47:42 +0300

    add authorization

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -13,18 +15,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // user auth emulation
-app.use((req, res, next) => {
-  req.user = {
-    _id: '630a4b017af1e4c56cff1c2c',
-  };
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '630a4b017af1e4c56cff1c2c',
+//   };
 
-  next();
-});
+//   next();
+// });
 
 app.post('/signup', createUser);
 app.post('/signin', login);
+
+app.use(auth);
 
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    next(err);
+  }
+
+  req.user = payload;
+
+  next();
+};
+
+module.exports = auth;
