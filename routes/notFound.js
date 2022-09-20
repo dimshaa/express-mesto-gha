9ef8149a3ff15a184d3d3f3e8054a0955b9c2a9e
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-09-20 23:10:14 +0300

    fix not found router

--- routes/notFound.js
+++ routes/notFound.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { NOT_FOUND_ERROR } = require('../utils/errorcodes');
+const NotFoundError = require('../utils/errors/NotFoundError');
 
-router.all('*', (req, res) => {
-  res.status(NOT_FOUND_ERROR).send({ message: `${res.statusCode} - such directory doesn't exist` });
+router.all('*', (req, res, next) => {
+  next(new NotFoundError('Such directory doesn\'t exist'));
 });
 
 module.exports = router;
